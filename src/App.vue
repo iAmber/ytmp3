855a1245397952f5827577edb6e27bed16fc0ce1
amber <957890374@qq.com>
2021-11-10 22:37:07 +0800

    add scroll to top

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <div id="app">
+      <router-view/>
+    </div>
+    <div class="back-top" @click="backTop" v-if="showBackTop">
+      <img src="./assets/img/top.png" />
+    </div>
   </div>
 </template>
+
+<script>
+// import Lazyload from 'vant/lib/lazyload';
+export default {
+  data() {
+    return {
+      showBackTop: false,
+    };
+  },
+  methods: {
+    backTop() {
+      window.app.scrollIntoView();
+    },
+  },
+  mounted() {
+    window.onscroll = () => {
+      if (window.scrollY > 300) {
+        this.showBackTop = true;
+      } else {
+        this.showBackTop = false;
+      }
+    };
+  },
+};
+</script>
 
 <style>
 @import url('./assets/css/font.css');
 html, body {
   margin: 0;
   padding: 0;
+}
+.back-top{
+  position: fixed;
+  bottom: 24px;
+  right: 24px;
+  height: 84px;
+  width: 84px;
 }
 </style>
